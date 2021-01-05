47b1a144c0eebe7e06ddf9777ed07b2dda8b3ee6
Gabriel Loureiro <gabrieloureirof@gmail.com>
2021-01-05 15:28:52 -0300

    connection with typeORM started

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import Routes from './routes'
 import express from 'express'
 
-require('./database')
+import "./database";
 
 const app = express()
 
 app.use(express.json())
 app.use(Routes)
 
-app.listen(3333)
+app.listen(3333, () => {
+  console.log("SERVER IS UP!")
+})
